de8f0012512f653d94943b18aabae1fe7d730f80
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-16 12:02:16 +0000

    Basic - Draw and Player1 win conditions

--- rps.js
+++ rps.js
@@ -1,6 +1,9 @@
 function rockPaperScissors(player1, player2) {
-
-  
+  if (player1===player2) {
+    return 'draw'
+  } else if ((player1 === 'paper' && player2 === 'rock') || (player1 === 'scissors' && player2 == 'paper') || (player1 === 'rock' && player2 === 'scissors')) {
+    return 'player1'
+  }
    
 }
 
@@ -9,8 +12,8 @@ function rockPaperScissors(player1, player2) {
 
 
 
-
-
+// Testing
+console.log(rockPaperScissors('paper','rock'))
 
 
 // Leave this code here for the automated tests

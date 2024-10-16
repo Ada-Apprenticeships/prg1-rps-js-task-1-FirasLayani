c2f5d24892ebe9e19aaa60217f08ea2e1626ab11
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-16 13:54:08 +0000

    Basic - Complete using Selection

--- rps.js
+++ rps.js
@@ -3,17 +3,21 @@ function rockPaperScissors(player1, player2) {
     return 'draw'
   } else if ((player1 === 'paper' && player2 === 'rock') || (player1 === 'scissors' && player2 == 'paper') || (player1 === 'rock' && player2 === 'scissors')) {
     return 'player1'
+  } else if ((player1 === 'paper' && player2 === 'scissors') || (player1 === 'scissors' && player2 == 'rock') || (player1 === 'rock' && player2 === 'paper')) {
+    return 'player2'
   }
    
 }
 
-
-
-
-
-
 // Testing
-console.log(rockPaperScissors('paper','rock'))
+
+// Player 1 wins: paper-rock, scissors-paper, rock-scissors
+// Player 2 wins: paper-scissors, scissors-rock, rock-paper
+
+// const readline = require('readline-sync')
+// const input1 = readline.question('Player 1: rock, paper, or scissors: ')
+// const input2 = readline.question('Player 2: rock, paper, or scissors: ')
+// console.log(rockPaperScissors(input1,input2))
 
 
 // Leave this code here for the automated tests

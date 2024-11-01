e4e67d6e60b2997f46ceee053a0126557231c88b
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-11-01 17:58:57 +0000

    Improved spacing

--- rps.js
+++ rps.js
@@ -1,22 +1,19 @@
 function rockPaperScissors(player1, player2) {
-  
-  // Dictionary containing weapons as keys and the weapons it beats as strings in an array
+  // Dictionary containing weapons as keys and the weapons it defeats as strings in an array
   const winConditions = {
-    rock: ['scissors','lizard'],
+    rock: ['scissors', 'lizard'],
     paper: ['rock', 'spock'],
     scissors: ['paper', 'lizard'],
     lizard: ['spock', 'paper'],
-    spock: ['scissors', 'rock']
+    spock: ['scissors', 'rock'],
   };
 
-  if (player1===player2) {
-    // If player 1 and player 2's choices are the same, it is a draw
+  if (player1 === player2) {
+    // If player 1 and player 2's choices are the same, return a draw
     return 'draw';
-
   } else if (winConditions[player1].includes(player2)) {
     // Otherwise if player 1's weapon (as a key in the dictionary/object) has player 2's weapon as a corresponding value, player 1 wins
     return 'player1';
-
   } else {
     // Otherwise player 2 wins
     return 'player2';
@@ -26,4 +23,4 @@ function rockPaperScissors(player1, player2) {
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
